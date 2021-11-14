51b97a696dce5236b20e1a42b55e5757047c8de4
Dylan <dylanheathcote2004@gmail.com>
2021-11-14 19:15:21 +0000

    setup

---
+++ Block.java
@@ -0,0 +1,20 @@
+import java.security.MessageDigest;
+import java.util.Date;
+
+
+public class Block {
+	
+	private String version;
+	private Date Timestamp;
+	private String hash;
+	private String previousHash;
+	private String data;
+
+
+
+
+
+
+
+
+}
